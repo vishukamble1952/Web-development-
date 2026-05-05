4e74687976b32232ade69e59615946d78a4b449b
vishukamble1952 <vishukamble1952@gmail.com>
2026-05-05 23:20:32 +0530

    Add HTML frameset to Exp8.cpp

---
+++ Exp8.cpp
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Frameset</title>
+</head>
+<frameset cols="25%, 25%, 25%, 25%">
+    <frame src="file:///C:/Users/samru/Desktop/WEBDEVELOPMENT/Exp1.html">
+    <frame src="file:///C:/Users/samru/Desktop/WEBDEVELOPMENT/Exp4.html">
+    <frame src="file:///C:/Users/samru/Desktop/WEBDEVELOPMENT/Exp6.html">
+    <frame src="file:///C:/Users/samru/Desktop/WEBDEVELOPMENT/Exp7.html">
+    
+</frameset>
+</html>
